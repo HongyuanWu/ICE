1dab44acdb9f246cc8e06395174db7b17e8dd3f6
Yu Sun <icannotendure666@gmail.com>
2020-06-25 21:40:53 +0800

    change ML HPs to optimized

--- R/ICE_cv.R
+++ R/ICE_cv.R
@@ -11,53 +11,42 @@
 #' @param method method for imputation, either "RF" for random forests, "KNN" for K-nearest neighbor or
 #' "SVM" for support vector machines.
 #' @param num number of informative protein coding genes to be used in constructing imputation model.
-#' Default is 50 genes.
+#' Default is 100 genes.
 #' @param folds number specifying folds (k) of cross validation to obtain imputation accuracy.
 #' Default is k=10.
 #' @param ... optional parameters that can be passed on to the machine-learning functions
 #' RF (\link[randomForest]{randomForest}), KNN (\link[FNN]{knn.reg}) or SVM(\link[e1071]{svm})
 #' @param filter if training datasets should be filtered? Default is TRUE, but should be set to FALSE when called in loop.
+#' @param cor.method a character string indicating
+#' which correlation coefficient is to be used for the correlation test.
+#' Default is "spearman", could also be "pearson".
 #'
-#' @return a matrix with three values corresponding to Spearman's correlation coefficient,
+#' @return a matrix with three values corresponding to correlation coefficient,
 #' P-value of the fit and root mean squared error (RMSE).
 #'
 #' @examples
 #' data(iMIRAGE.datasets)
-#' ICE_cv(GA.pcg, GA.mir, gene.index="hsa-let-7c", method="KNN", num=50)
-#' ICE_cv(GA.pcg, GA.mir, gene.index=25, method="KNN", num=50)
-#'
-#' @import randomForest
-#' @import FNN
-#' @import e1071
+#' ICE_cv(GA.pcg, GA.mir, gene.index="hsa-let-7c", method="KNN", num=100)
+#' ICE_cv(GA.pcg, GA.mir, gene.index=25, method="KNN", num=100)
 #'
 #' @export ICE_cv
-ICE_cv <- function (train.pcg, train.circ, gene.index, num = 50, method = "KNN", folds = 10, filter = TRUE, ...) {
+ICE_cv <- function (train.pcg, train.circ, gene.index, num = 50,
+                    method = "KNN", folds = 10, filter = TRUE, cor.method = 'spearman', ...) {
 
-  if (mode(gene.index)!="numeric" & mode(gene.index)!="character") stop ("Error: circRNA not found in training dataset. Please check gene name or rownumber")
-  if (mode(gene.index)=="numeric" & gene.index > ncol(train.circ))  stop ("Error: circRNA not found in training dataset. Please check ID or rownumber")
-  if (mode(gene.index)=="character" & is.na (match (gene.index, colnames(train.circ)))) stop ("Error: circRNA not found. Please check ID or rownumber")
+  if (mode(gene.index) != "numeric" & mode(gene.index) != "character") stop ("Error: circRNA not found in training dataset. Please check gene name or rownumber")
+  if (mode(gene.index) == "numeric" & gene.index > ncol(train.circ))  stop ("Error: circRNA not found in training dataset. Please check ID or rownumber")
+  if (mode(gene.index) == "character" & is.na (match (gene.index, colnames(train.circ)))) stop ("Error: circRNA not found. Please check ID or rownumber")
 
   if (filter) {
     train.pcg <- pre_process(train.pcg)
-    train.circ <- filter_circ(train.circ)
+    train.circ <- scale(filter_circ(train.circ))
   }
+
+  if (ncol(train.pcg) >= num & method != 'lasso' & method != "EN") train.pcg <- scale(corf(train.pcg, train.circ, gene.index, num))
+  if (ncol(train.pcg) < num) train.pcg <- scale(train.pcg)
 
   cv.res <- matrix(nrow=folds, ncol=3)
-  colnames (cv.res) <- c("PCC", "P-Value", "RMSE")
-
-  if (num >= 50 & ncol(train.pcg) >=50 ) x <- corf(train.pcg, train.circ, gene.index, num)
-  if (ncol(train.pcg) < 50) x <- train.pcg
-
-  if (mode(gene.index)=="numeric") {
-    if (num >= 50 & ncol(train.pcg) >=50 ) train.pcg <- corf(train.pcg, train.circ, gene.index, num)
-    if (ncol(train.pcg) < 50) train.pcg <- train.pcg
-  }
-  if (mode(gene.index)=="character") {
-    if (num >= 50 & ncol(train.pcg) >=50 ) train.pcg <- corf(train.pcg, train.circ, gene.index, num)
-    if (ncol(train.pcg) < 50) train.pcg <- train.pcg
-  }
-
-  cat("\nRunning ", folds,"-folds cross-validation...", sep="")
+  colnames (cv.res) <- c("CC", "P-Value", "RMSE")
 
   RS <- nrow(train.pcg)
   while (RS %% folds != 0) {
@@ -66,8 +55,6 @@ ICE_cv <- function (train.pcg, train.circ, gene.index, num = 50, method = "KNN",
   kgrp <- split(sample(1:RS, RS, replace=F), 1:folds)
 
   for (k in 1:folds) {
-
-    cat("\nIteration ", k, sep="")
 
     ind <- unlist(kgrp[[k]])
     x <- train.pcg [-ind, ]
@@ -85,9 +72,9 @@ ICE_cv <- function (train.pcg, train.circ, gene.index, num = 50, method = "KNN",
 
     if(method=="RF") {
       #randomForest
-      imp.rf <- randomForest(x, y, ntree=250, ...)
+      imp.rf <- randomForest(x, y, ntree=500, mtry=33, nodesize=1)
       predict.y <- predict(imp.rf, testx)
-      r.rf <- suppressWarnings(cor.test(predict.y, actual.y, method="spearman"))
+      r.rf <- suppressWarnings(cor.test(predict.y, actual.y, method = cor.method))
       rmse.rf <- sqrt(mean((actual.y-predict.y)^2))
       cv.res[k, 1:3] <- c(r.rf$estimate, r.rf$p.value, rmse.rf)
       next()
@@ -95,8 +82,8 @@ ICE_cv <- function (train.pcg, train.circ, gene.index, num = 50, method = "KNN",
 
     if (method=="KNN") {
       #knn regression
-      knn.fit <- knn.reg(train = x, test = testx, y = y, k=50, ...)
-      r.knn <- suppressWarnings(cor.test(knn.fit$pred, actual.y, method="spearman"))
+      knn.fit <- knn.reg(train = x, test = testx, y = y, k = 1, ...)
+      r.knn <- suppressWarnings(cor.test(knn.fit$pred, actual.y, method = cor.method))
       rmse.knn <- sqrt(mean((actual.y - knn.fit$pred)^2))
       cv.res[k, 1:3] <- c(r.knn$estimate, r.knn$p.value, rmse.knn)
       next()
@@ -104,31 +91,27 @@ ICE_cv <- function (train.pcg, train.circ, gene.index, num = 50, method = "KNN",
 
     if (method=="SVM") {
       #svm regression
-      imp.svm <- svm(x, y, ...)
+      imp.svm <- svm(x, y, cost = 2222, gamma = 0.12, type = 'eps-regression', kernel = 'radial')
       predict.y <- predict(imp.svm, testx)
-      r.svm <- suppressWarnings(cor.test(predict.y, actual.y, method="spearman"))
+      r.svm <- suppressWarnings(cor.test(predict.y, actual.y, method = cor.method))
       rmse.svm <- sqrt(mean((actual.y-predict.y)^2))
       cv.res[k, 1:3] <- c(r.svm$estimate, r.svm$p.value, rmse.svm)
       next()
     }
 
     if (method=="lasso") {
-      cv.lasso <- cv.glmnet(x, y, alpha = 1, family = 'poisson')
-      lasso.model <- glmnet(x, y, alpha = 1, family = 'poisson')
-      predict.y <- predict.glmnet(lasso.model, s = cv.lasso$lambda.min, newx = testx)
-      r.lasso <- suppressWarnings(cor.test(predict.y, actual.y, method="spearman"))
+      lasso.model <- glmnet(x, y, alpha = 1, family = 'gaussian')
+      predict.y <- predict.glmnet(lasso.model, s = 0.01530921, newx = testx)
+      r.lasso <- suppressWarnings(cor.test(predict.y, actual.y, method = cor.method))
       rmse.lasso <- sqrt(mean((actual.y - predict.y) ^ 2))
       cv.res[k, 1:3] <- c(r.lasso$estimate, r.lasso$p.value, rmse.lasso)
       next()
     }
 
     if (method == "EN") {
-      en.model <- suppressWarnings(train(
-        x, y, method = 'glmnet',
-        trControl = trainControl("cv", number = 10),
-        tuneLength = 100))
-      predict.y <- predict(en.model, testx)
-      r.en <- suppressWarnings(cor.test(predict.y, actual.y, method="spearman"))
+      en.model <- glmnet(x, y, alpha = 0.9909091, family = 'gaussian')
+      predict.y <- predict.glmnet(en.model, s = 0.03019828, newx = testx)
+      r.en <- suppressWarnings(cor.test(predict.y, actual.y, method = cor.method))
       rmse.en <- sqrt(mean((actual.y - predict.y) ^ 2))
       cv.res[k, 1:3] <- c(r.en$estimate, r.en$p.value, rmse.en)
       next()
@@ -136,15 +119,14 @@ ICE_cv <- function (train.pcg, train.circ, gene.index, num = 50, method = "KNN",
 
     if (method == 'PCR') {
       temp.df <- data.frame(x, y)
-      pcr.model <- pcr(y ~ ., data = temp.df, scale =TRUE, validation = "CV")
-      predict.y <- predict(pcr.model, x, ...)
-      r.pcr <- suppressWarnings(cor.test(predict.y, actual.y, method="spearman"))
+      pcr.model <- pcr(y ~ ., data = temp.df, scale =TRUE, validation = "CV", ...)
+      predict.y <- predict(pcr.model, testx, ...)[, , ]
+      r.pcr <- suppressWarnings(cor.test(predict.y, actual.y, method = cor.method))
       rmse.pcr <- sqrt(mean((actual.y - predict.y) ^ 2))
-      cv.res[k, 1:3] <- c(rmse.pcr$estimate, rmse.pcr$p.value, rmse.pcr)
+      cv.res[k, 1:3] <- c(r.pcr$estimate, r.pcr$p.value, rmse.pcr)
       next()
     }
   }
 
-  cat("\nCross-validation complete\n")
   return (cv.res)
 }
